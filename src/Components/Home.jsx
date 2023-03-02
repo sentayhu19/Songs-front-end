84673f4893d6aa28d0b4dc447f9e052a9c6c31f6
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2023-03-02 18:31:19 +0300

    Some styling updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import SongList from './SongList'
 const Home = () => {
   return (
     <div>
+      <h1 className='m-10 text-3xl  border-2 w-[10%]'><span className='text-green-500'>Addis</span> Music</h1>
       <SongList/>
     </div>
   )
--- src/Components/SongList.jsx
+++ src/Components/SongList.jsx
@@ -3,41 +3,77 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAllSongsSuccess } from '../redux/song/songSlice';
 import EditSongForm from './EditSongForm';
 
+import { ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
+import { Box } from 'rebass/styled-components';
+
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+`;
+
+const Th = styled.th`
+  background-color: #f2f2f2;
+  text-align: left;
+  padding: 8px;
+`;
+
+const Td = styled.td`
+  text-align: left;
+  padding: 8px;
+`;
+
+const Button = styled.button`
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  cursor: pointer;
+`;
 
 const SongList = () => {
-    const dispatch = useDispatch();
-    const { songs } = useSelector((state) => state.songs);
-   useEffect(() => {
+  const dispatch = useDispatch();
+  const { songs } = useSelector((state) => state.songs);
+
+  useEffect(() => {
     console.log("init useEFFECt getALL Songs")
     dispatch({type: "songs/getAllSongs"}); 
-    }, [dispatch]);
-console.log("SONGS LIST", songs)
+  }, [dispatch]);
+
+  console.log("SONGS LIST", songs);
+
   return (
-    <div>
-      <table>
+    <Box>
+      <Table>
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Genre</th>
-            <th>Actions</th>
+            <Th>Title</Th>
+            <Th>Artist</Th>
+            <Th>Genre</Th>
+            <Th>Actions</Th>
           </tr>
         </thead>
         <tbody>
           {songs.songs ? songs.songs.map((song) => (
             <tr key={song.id}>
-              <td>{song.title}</td>
-              <td>{song.artist}</td>
-              <td>{song.genre}</td>
-              <td>
+              <Td>{song.title}</Td>
+              <Td>{song.artist}</Td>
+              <Td>{song.genre}</Td>
+              <Td>
                 <EditSongForm song={song} />
-                <button>Delete</button>
-              </td>
+                <Button>Delete</Button>
+              </Td>
             </tr>
           )): ''}
         </tbody>
-      </table>
-    </div>
+      </Table>
+    </Box>
   );
 };
 
